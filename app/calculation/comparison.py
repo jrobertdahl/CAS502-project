cc3c76b7a211351eb209b9da86695285918076f3
Sophia McConnell <scm.929@gmail.com>
2024-01-31 19:56:20 -0500

    Create first iteration of tree comparison function

---
+++ app/calculation/comparison.py
@@ -0,0 +1,11 @@
+#tree comparison
+#eventually all comparison calculations will be in one function
+
+
+def tree_comparison(jet_emission):
+  annual_tree_absorption_lbs = 48 #from arbor day foundation
+  trees_needed = jet_emission/annual_tree_absorption_lbs
+  trees_needed = int(trees_needed)
+  tree_output = f"It will take {trees_needed} trees approximately 1 year to remove this CO2 from the atmosphere."
+  return tree_output
+  
