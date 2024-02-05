9f199faa2845571c3dd070756dd2feb0df9fcecd
Sophia McConnell <scm.929@gmail.com>
2024-02-04 19:45:35 -0500

    create first draft of LA to NY comparison calculation

--- app/calculation/comparison.py
+++ app/calculation/comparison.py
@@ -16,6 +16,12 @@ def tree_comparison(jet_emission):
   #Compact Car
   #using E10 fuel
   #3.396 km between LA and NY
+def LA_NY_drive_comparison(jet_emission):
+  trip_emissions_mt = 0.997
+  trips_needed = jet_emission/trip_emissions_mt
+  trips_needed = int(trips_needed)
+  trip_output = f"You would have to drive from LA to NY {trips_needed} times to emit this same amount."
+  return trip_output
 
 
 #average person emission calculations
