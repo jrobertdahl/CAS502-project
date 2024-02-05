84f81ead70bbf3d3df0f7e12a07b25e6297c33a9
Sophia McConnell <scm.929@gmail.com>
2024-02-04 19:40:27 -0500

    converting lbs CO2 to mt for all three comparisons

--- app/calculation/comparison.py
+++ app/calculation/comparison.py
@@ -1,19 +1,22 @@
 #tree comparison
 #eventually all comparison calculations will be in one function
-
+#a mature tree absorbs 48 lbs CO2 per year = 0.02 mt
 
 def tree_comparison(jet_emission):
-  annual_tree_absorption_lbs = 48 #from arbor day foundation
-  trees_needed = jet_emission/annual_tree_absorption_lbs
+  annual_tree_absorption_mt = 0.02 #from arbor day foundation
+  trees_needed = jet_emission/annual_tree_absorption_mt
   trees_needed = int(trees_needed)
   tree_output = f"It will take {trees_needed} trees approximately 1 year to remove this CO2 from the atmosphere."
   return tree_output
   
 
 
-#driving from LA to NY comparison, 2200lbs CO2 
+#driving from LA to NY comparison, 2200lbs CO2 = 0.997 mt
 #Assumptions:
   #Compact Car
   #using E10 fuel
   #3.396 km between LA and NY
 
+
+#average person emission calculations
+# average american emits 16 tons every year = 14.5 mt
