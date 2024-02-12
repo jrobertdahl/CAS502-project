0d5f80ff4377c0b365cd95a50e18a8fa65c73c31
Sophia McConnell <scm.929@gmail.com>
2024-02-12 17:29:41 -0500

    convert average american emissions to months if years is less than 1 year

--- app/calculation/comparison.py
+++ app/calculation/comparison.py
@@ -1,14 +1,11 @@
 import random
-
-def get_emissions_comparison_text(jet_emission):
-  comparisons = ['tree_comparison', 'LA_NY_drive_comparison', 'average_american_emissions_comparison']
+def emissions_comparison_calculation(jet_emission):
+  comparisons = [tree_comparison, LA_NY_drive_comparison, average_american_emissions_comparison]
   selection = random.choice(comparisons)
-  
-  if selection == 'tree_comparison':
-
+  if selection == tree_comparison:
     #calculating how many trees are needed to absorb jet_emission
     #from arbor day foundation: a mature tree absorbs 48 lbs CO2 per year = 0.02 mt CO2
-
+   
     annual_tree_absorption_mt = 0.02
     trees_needed = jet_emission/annual_tree_absorption_mt
     trees_needed = int(trees_needed)
@@ -16,24 +13,29 @@ def get_emissions_comparison_text(jet_emission):
     
     return tree_output
   
-  elif selection == 'LA_NY_drive_comparison':
-
+  elif selection == LA_NY_drive_comparison:
     #driving from LA to NY comparison, 2200lbs CO2 = 0.997 mt
     #Assumptions: compact car, using E10 fuel, there are 3,396 km between LA and NY
-
+    
     trip_emissions_mt = 0.997
     trips_needed = jet_emission/trip_emissions_mt
     trips_needed = int(trips_needed)
     trip_output = f"You would have to drive from LA to NY {trips_needed} times to emit this same amount."
     
     return trip_output
-  
+ 
   else:
-
+    
     #an average american emits 16 tons CO2 every year = 14.5 mt
-
+    
     american_emissions_mt = 14.5
     years_needed = jet_emission/american_emissions_mt
-    years_output = f"It would take an average American {years_needed} years to emit the same amount."
-    
-    return years_output
+    if years_needed <1:
+      months_needed = years_needed * 12
+      months_needed = int(months_needed)
+      months_output = f"It would take an average American {months_needed} months to emit the same amount."
+      return months_output
+    else:
+      years_needed = round(years_needed,2)
+      years_output = f"It would take an average American {years_needed} years to emit the same amount."
+      return years_output
