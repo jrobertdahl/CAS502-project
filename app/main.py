91450b510189ee678ef4243f2a8224d82c1d7343
Sophia McConnell <scm.929@gmail.com>
2024-02-12 16:50:55 -0500

    update miles traveled to km traveled in main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from calculation.comparison import get_emissions_comparison_text
 from posting.postToX import post_to_x
 
 name = ""
-miles_traveled = 0
+km_traveled = 0
 tons_co2_emitted = 0
 base_post_text = ""
 comparison_text = ""
@@ -13,13 +13,13 @@ with open('dataCollection/testData/flightInfo.json') as json_file:
     data = json.load(json_file)
  
     name = data['test_data_1']['name']
-    miles_traveled = data['test_data_1']['miles_traveled']
+    km_traveled = data['test_data_1']['km_traveled']
 
 # pull in the comparison text
 comparison_text = get_emissions_comparison_text(1000)
 
 # build the full post text
-base_post_text = f'TEST - {name} just flew {miles_traveled} miles in their private jet, emitting {tons_co2_emitted} tons of CO₂ into the atmosphere. ' + comparison_text
+base_post_text = f'TEST - {name} just flew {km_traveled} km in their private jet, emitting {tons_co2_emitted} tons of CO₂ into the atmosphere. ' + comparison_text
 
 # post it to x
 post_to_x(base_post_text)
