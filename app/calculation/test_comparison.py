3dd9c94700a684af068d2070feae3351dd0e17ca
Sophia McConnell <scm.929@gmail.com>
2024-02-17 19:39:14 -0500

    resolve conflict in test_comparison.py

--- app/calculation/test_comparison.py
+++ app/calculation/test_comparison.py
@@ -6,35 +6,35 @@ class test_emissions_comparison_calculation(unittest.TestCase):
 
     @patch('comparison.random.choice')
     def test_tree_comparison(self, mock_choice):
-        mock_choice.return_value = "tree_comparison"
+        mock_choice.return_value = 'tree_comparison'
         jet_emission = 5.5
         output = comparison.emissions_comparison_calculation(jet_emission)
         self.assertEqual(output, "It will take 275 trees approximately 1 year to remove this CO2 from the atmosphere.")
 
     @patch('comparison.random.choice')
     def test_LA_NY_comparison(self, mock_choice):
-        mock_choice.return_value = "LA_NY_drive_comparison"
+        mock_choice.return_value = 'LA_NY_drive_comparison'
         jet_emission = 5.5
         output = comparison.emissions_comparison_calculation(jet_emission)
         self.assertEqual(output, "You would have to drive from LA to NY 5 times to emit this same amount.")
 
     @patch('comparison.random.choice')
     def test_average_american_comparison_less_than_one_year(self, mock_choice):
-        mock_choice.return_value = "average_american_emissions_comparison"
+        mock_choice.return_value = 'average_american_emissions_comparison'
         jet_emission = 5.5
         output = comparison.emissions_comparison_calculation(jet_emission)
         self.assertEqual(output, "It would take an average American 4 months to emit the same amount.")
 
     @patch('comparison.random.choice')
     def test_average_american_comparison_one_year(self, mock_choice):
-        mock_choice.return_value = "average_american_emissions_comparison"
+        mock_choice.return_value = 'average_american_emissions_comparison'
         jet_emission = 14.5
         output = comparison.emissions_comparison_calculation(jet_emission)
         self.assertEqual(output, "It would take an average American 1.0 years to emit the same amount.")
 
     @patch('comparison.random.choice')
     def test_average_american_comparison_more_than_one_year(self, mock_choice):
-        mock_choice.return_value = "average_american_emissions_comparison"
+        mock_choice.return_value = 'average_american_emissions_comparison'
         jet_emission = 20
         output = comparison.emissions_comparison_calculation(jet_emission)
         self.assertEqual(output, "It would take an average American 1.38 years to emit the same amount.")
