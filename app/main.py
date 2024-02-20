a0d8d8cdf55232b7da651c924815bcdf1b0228a0
Sophia McConnell <scm.929@gmail.com>
2024-02-19 20:33:11 -0500

    add emissions calculation to main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
-from calculation.comparison import get_emissions_comparison_text
 from data_collection.get_stand_in_data import get_stand_in_data,select_data_value
+from calculation.emissions_calculation import calculate_emissions
+from calculation.comparison import get_emissions_comparison_text
 from posting.postToX import post_to_x
 
 name = ""
@@ -15,8 +16,11 @@ temp_data_value = select_data_value(sys.argv)
 # pull in the individual's name and the distance of a recent flight - stand in data for now
 name,km_traveled = get_stand_in_data(temp_data_value)
 
+# calculate emissions based on an average jet for km traveled
+metric_tons_co2_emitted = calculate_emissions(km_traveled)
+
 # pull in the comparison text
-comparison_text = get_emissions_comparison_text(1000)
+comparison_text = get_emissions_comparison_text(metric_tons_co2_emitted)
 
 # build the full post text
 post_text = f'TEST - {name} just flew {km_traveled} km in their private jet, emitting {metric_tons_co2_emitted} metric tons of CO₂ into the atmosphere. ' + comparison_text
